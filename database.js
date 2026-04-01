03014b786d1c2bcc72b04d15bb0025e6e00c5bdc
Juan Esteban Gallardo Alava <75323396+Juangallardo-rgb@users.noreply.github.com>
2026-04-01 10:22:12 -0500

    DB fix db connection

--- database.js
+++ database.js
@@ -1,10 +1,9 @@
-import pkg from 'pg';
-const { Pool } = pkg;
+const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // importante para Supabase
+    rejectUnauthorized: false,
   },
 });
 
